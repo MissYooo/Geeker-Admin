671c4bf0b1f56c1759f1601994516263fa853924
韩强 <1915217655@qq.com>
2024-08-29 09:56:55 +0800

    feat(project): ✨ 精简框架

--- vite.config.ts
+++ vite.config.ts
@@ -48,31 +48,6 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     plugins: createVitePlugins(viteEnv),
     esbuild: {
       pure: viteEnv.VITE_DROP_CONSOLE ? ["console.log", "debugger"] : []
-    },
-    build: {
-      outDir: "dist",
-      minify: "esbuild",
-      // esbuild 打包更快，但是不能去除 console.log，terser打包慢，但能去除 console.log
-      // minify: "terser",
-      // terserOptions: {
-      // 	compress: {
-      // 		drop_console: viteEnv.VITE_DROP_CONSOLE,
-      // 		drop_debugger: true
-      // 	}
-      // },
-      sourcemap: false,
-      // 禁用 gzip 压缩大小报告，可略微减少打包时间
-      reportCompressedSize: false,
-      // 规定触发警告的 chunk 大小
-      chunkSizeWarningLimit: 2000,
-      rollupOptions: {
-        output: {
-          // Static resource classification and packaging
-          chunkFileNames: "assets/js/[name]-[hash].js",
-          entryFileNames: "assets/js/[name]-[hash].js",
-          assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
-        }
-      }
     }
   };
 });
